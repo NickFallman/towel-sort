95b3e45ded6b63a6009628d3bae6eb23f84983a2
Valentine Garbuzenko <nickf@tut.by>
2022-02-18 11:04:46 +0300

    fix: add get a size of unequal rows in matrix

--- src/index.js
+++ src/index.js
@@ -26,9 +26,9 @@ module.exports = function towelSort (matrix) {
   // check input params
   if((matrix === undefined) || (matrix.length === 0)) return sortedArr;
   const mtrxSz = matrix.length;
-  mtrxElmSz = matrix[0].length;
 
   for (i = 0; i < mtrxSz; i++) {
+    mtrxElmSz = matrix[i].length;
     for (j = 0; j < mtrxElmSz; j++) {
         curMtrxElm = matrix[i][i % 2 ? mtrxElmSz - j - 1 : j];
         // console.log(curMtrxElm);
