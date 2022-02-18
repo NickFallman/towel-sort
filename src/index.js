17c7a10be4479169e018a79b4113fcb8f00925b9
Valentine Garbuzenko <nickf@tut.by>
2022-02-18 10:54:59 +0300

    feat: create debug version of towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,43 @@
-
 // You should implement your task here.
 
+// module.exports = function towelSort (matrix) {
+//   return [];
+// }
+
+// const inMtrx = [
+//   [12, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 1],
+// ];
+
+// result = [ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12,];
+
+// const inMtrx = [
+//   1,
+//   2,
+//   4,
+//   3
+// ]
+
 module.exports = function towelSort (matrix) {
-  return [];
+  let sortedArr = [], i, j, curMtrxElm;
+  let mtrxElmSz = 0;
+  // console.log(matrix);
+
+  const mtrxSz = matrix.length;
+  mtrxElmSz = matrix[0].length;
+
+  for (i = 0; i < mtrxSz; i++) {
+    for (j = 0; j < mtrxElmSz; j++) {
+        curMtrxElm = matrix[i][i % 2 ? mtrxElmSz - j - 1 : j];
+        // console.log(curMtrxElm);
+        sortedArr.push(curMtrxElm);
+    }
+  }
+
+  return sortedArr;
 }
+
+// const res = towelSort(inMtrx);
+// const res = towelSort();
+// console.log(res);
